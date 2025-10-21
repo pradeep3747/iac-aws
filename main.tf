a2de6c7fe5dd92f9e96fc48847f1243d1ac30236
pradeep3747 <pradeep37047@gmail.com>
2025-10-21 13:39:18 +0530

    changes

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
 }
 
