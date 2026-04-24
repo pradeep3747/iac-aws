5f6d567fff9f952fe219a5908bde9f5cdeb49ccf
pradeep3747 <134362661+pradeep3747@users.noreply.github.com>
2026-04-24 18:20:12 +0530

    Change security group reference to use ID

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.ec2_sg.name]
+  security_groups = [aws_security_group.ec2_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   tags = {
     Name = "CodeDeployEC2"
